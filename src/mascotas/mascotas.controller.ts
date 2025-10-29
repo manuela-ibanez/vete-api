985e15dc1498e1ec645f5d135df8b468756b1611
Manuela Ibañez <mibaniez@alumnos.exa.unicen.edu.ar>
2025-10-29 19:41:18 -0300

    se añadió los avisos de errores

--- src/mascotas/mascotas.controller.ts
+++ src/mascotas/mascotas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, HttpStatus, HttpException, Put } from '@nestjs/common';
 import { MascotasService } from './mascotas.service';
 import { CreateMascotaDto } from './dto/create-mascota.dto';
 import { UpdateMascotaDto } from './dto/update-mascota.dto';
@@ -11,9 +11,9 @@ export class MascotasController {
     async create(@Body() createMascotaDto: CreateMascotaDto) {
       try {
         const mascota = await this.mascotasService.create(createMascotaDto);
-        return { mensaje: 'Mascota creada correctamente', mascota }; // Devuelve un mensaje y el usuario creado.
+        return { mensaje: 'Mascota creada correctamente', mascota }; // Devuelve un mensaje y la mascota creada.
       } catch (error) {
-        throw new HttpException('Error al crear la mascota', HttpStatus.INTERNAL_SERVER_ERROR);
+        throw new HttpException('Error al crear la mascota', HttpStatus.BAD_REQUEST);
       }
     }
 
@@ -24,18 +24,14 @@ export class MascotasController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    try {
       const mascota = await this.mascotasService.findOne(+id);
       if (!mascota) {
         throw new HttpException('Mascota no encontrada', HttpStatus.NOT_FOUND);
       }
       return mascota;
-    } catch (error) {
-      throw new HttpException('Error al obtener la mascota', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
   }
 
-  @Patch(':id')
+  @Put(':id')
   async update(@Param('id') id: string, @Body() updateMascotaDto: UpdateMascotaDto) {
     try {
       const mascota = await this.mascotasService.findOne(+id);
@@ -43,23 +39,22 @@ export class MascotasController {
         throw new HttpException('Mascota no encontrada', HttpStatus.NOT_FOUND);
       }
       const updatedMascota = await this.mascotasService.update(+id, updateMascotaDto);
+      if (!updatedMascota) {
+        throw new HttpException('Error al actualizar la mascota', HttpStatus.INTERNAL_SERVER_ERROR);
+      }
       return { mensaje: 'Mascota actualizada correctamente', mascota: updatedMascota };
     } catch (error) {
-      throw new HttpException('Error al actualizar la mascota', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('Error al actualizar la mascota', HttpStatus.BAD_REQUEST);
     }
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
-    try {
       const mascota = await this.mascotasService.findOne(+id);
       if (!mascota) {
         throw new HttpException('Mascota no encontrada', HttpStatus.NOT_FOUND);
       }
       await this.mascotasService.remove(+id);
       return { mensaje: 'Mascota eliminada correctamente' };
-    } catch (error) {
-      throw new HttpException('Error al eliminar la mascota', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
   }
 }
--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, HttpStatus, HttpException, Put } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
@@ -13,7 +13,7 @@ export class UsuariosController {
       const usuario = await this.usuariosService.create(createUsuarioDto);
       return { mensaje: 'Usuario creado correctamente', usuario }; // Devuelve un mensaje y el usuario creado.
     } catch (error) {
-      throw new HttpException('Error al crear el usuario', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('Error al crear el usuario', HttpStatus.BAD_REQUEST);
     }
   }
 
@@ -24,18 +24,14 @@ export class UsuariosController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    try {
       const usuario = await this.usuariosService.findOne(+id);
       if (!usuario) {
         throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
       }
       return usuario;
-    } catch (error) {
-      throw new HttpException('Error al obtener el usuario', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
   }
 
-  @Patch(':id')
+  @Put(':id')
   async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     try {
       const usuario = await this.usuariosService.findOne(+id);
@@ -45,21 +41,17 @@ export class UsuariosController {
       const updatedUsuario = await this.usuariosService.update(+id, updateUsuarioDto);
       return { mensaje: 'Usuario actualizado correctamente', usuario: updatedUsuario };
     } catch (error) {
-      throw new HttpException('Error al actualizar el usuario', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('Error al actualizar el usuario', HttpStatus.BAD_REQUEST);
     }
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
-    try {
       const usuario = await this.usuariosService.findOne(+id);
       if (!usuario) {
         throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
       }
       await this.usuariosService.remove(+id);
       return { mensaje: 'Usuario eliminado correctamente' };
-    } catch (error) {
-      throw new HttpException('Error al eliminar el usuario', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
   }
 }

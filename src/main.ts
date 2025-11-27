816bbf2b759200f65d552f2e3000aec098622d45
Manuela Ibañez <mibaniez@alumnos.exa.unicen.edu.ar>
2025-11-26 21:36:03 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  app.enableCors();
+
+  //HABILITAR CORS PARA QUE ANGULAR PUEDA CONECTARSE
+  app.enableCors({
+    origin: 'http://localhost:4200', // AQUI ESTA CORRIENDO ANGULAR
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(3000)
 }
 bootstrap();
